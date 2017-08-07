6d81a416ca01eec5559922aa42d53a4b4b9b3200
Shellin_F <904222810@qq.com>
2017-08-07 17:18:49 +0800

    first commit

---
+++ customerInput/js/Cart.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Shellin on 2017/7/29.
+ */
+var vm=new Vue({
+    el:"#app",
+    data:{
+        msg:"23",
+        seen:true,
+        games:[
+            {title:"lol",time:'1'},
+            {title:"dota2",time:'2'},
+            {title:"code",time:'10'}
+        ],
+        list:[111,222,333]
+    },
+    filters:{
+    },
+    mounted:function(){
+
+    },
+    methods: {
+
+    }
+});
